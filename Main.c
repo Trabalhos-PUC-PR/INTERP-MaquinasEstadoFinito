6c8aefb4d4cc323c514e2c2545d7deb46fefeae9
kovalski-rgb <andre.kovalski@tecpuc.com.br>
2022-08-04 11:01:53 -0300

    Begin implementation of verifier function

--- Main.c
+++ Main.c
@@ -1,16 +1,58 @@
 #include <stdio.h>
 
+#define stringSize 256
+
+typedef enum 
+{ 
+    false = 0, 
+    true = 1
+} bool;
+
+bool isVerified(char *readString);
+
 int main(){
 
-    FILE* input;
+    FILE* inputFile;
+    int totalLines;
+    char readString[stringSize];
+    bool passesCheck = false;
 
-    input = fopen("input.txt", "r");
+    inputFile = fopen("input.txt", "r");
 
-    if(input == NULL){
+    
+    if(inputFile == NULL){
         printf("No file names \"input.txt\" found!");
-        return 1;
+        fclose(inputFile);
+        return -1;
     }
 
-    printf("file exists!");
+    fgets(readString, stringSize, inputFile);
+    sscanf(readString, "%d", &totalLines);
+    printf("lines: %i\n", totalLines);
+
+    for(int i = 0; i < totalLines; i++){
+        passesCheck = false;
+        fgets(readString, stringSize, inputFile);
+        
+        if(isVerified(readString) == true){
+            passesCheck = true;
+        }
+        
+        printf("#%u: %s | %s", i, (passesCheck==1)?"OK":"FAIL", readString);
+    }
+
+    fclose(inputFile);
     return 0;
+}
+
+bool isVerified(char* readString){
+    int length = strlen(readString);
+    int doLoop = true;
+
+    for(int i = 0; i < length; i++){
+        
+    }
+
+    printf("end");
+    return true;
 }
